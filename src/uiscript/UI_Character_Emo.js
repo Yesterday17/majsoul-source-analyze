ee82ffc622f23c9369d44f1ab7cbab0c9a0380d9
Yesterday17 <t@yesterday17.cn>
2019-05-04 20:45:48 +0800

    UI_Character_Emo es6ify

--- src/uiscript/UI_Character_Emo.js
+++ src/uiscript/UI_Character_Emo.js
@@ -1,55 +1,53 @@
-module.exports = function(uiscript) {
-  var newCharacterEmo = (function() {
-    function characterEmo(t) {
-      this.change_id = 0;
-      this.skin_path = "";
-      this.loaded = false;
-      this.me = t;
-      this.me.skin = "";
-    }
+class Character_Emo {
+  constructor(t) {
+    this.change_id = 0;
+    this.skin_path = "";
+    this.loaded = false;
+    this.me = t;
+    this.me.skin = "";
+  }
 
-    characterEmo.prototype.setSkin = function(t, type) {
-      let _this = this,
-        character = cfg.item_definition.character.get(t);
-      if (character) {
-        let skin_path = character.emo + "/" + type.toString() + ".png";
-        if (this.skin_path != skin_path) {
-          this.skin_path = skin_path;
-          this.change_id++;
+  setSkin(t, type) {
+    let _this = this;
+    let character = cfg.item_definition.character.get(t);
+    if (character) {
+      let skin_path = `${character.emo}/${type.toString()}.png`;
+      if (this.skin_path != skin_path) {
+        this.skin_path = skin_path;
+        this.change_id++;
 
-          let change_id = this.change_id,
-            resImgSkin = game.LoadMgr.getResImageSkin(skin_path);
-          if (resImgSkin == "") {
-            this.me.skin = game.LoadMgr.getResImageSkin(
-              "extendRes/emo/default.png"
-            );
-            this.loaded = false;
-            game.LoadMgr.loadResImage(
-              [this.skin_path],
-              Laya.Handler.create(this, function() {
-                if (_this.change_id == change_id) {
-                  _this.loaded = true;
-                  _this.me.skin = game.LoadMgr.getResImageSkin(_this.skin_path);
-                }
-              })
-            );
-          } else {
-            this.loaded = true;
-            this.me.skin = resImgSkin;
-          }
+        let change_id = this.change_id;
+        let resImgSkin = game.LoadMgr.getResImageSkin(skin_path);
+        if (resImgSkin == "") {
+          this.me.skin = game.LoadMgr.getResImageSkin(
+            "extendRes/emo/default.png"
+          );
+          this.loaded = false;
+          game.LoadMgr.loadResImage(
+            [this.skin_path],
+            Laya.Handler.create(this, () => {
+              if (_this.change_id == change_id) {
+                _this.loaded = true;
+                _this.me.skin = game.LoadMgr.getResImageSkin(_this.skin_path);
+              }
+            })
+          );
+        } else {
+          this.loaded = true;
+          this.me.skin = resImgSkin;
         }
-      } else this.clear();
-    };
-
-    characterEmo.prototype.clear = function() {
-      this.change_id++;
-      if (this.loaded && this.skin_path != "") {
-        Laya.loader.clearTextureRes(this.skin_path);
-        this.loaded = false;
-        this.skin_path = "";
       }
-    };
-    return characterEmo;
-  })();
-  uiscript.UI_Character_Emo = newCharacterEmo;
-};
+    } else this.clear();
+  }
+
+  clear() {
+    this.change_id++;
+    if (this.loaded && this.skin_path != "") {
+      Laya.loader.clearTextureRes(this.skin_path);
+      this.loaded = false;
+      this.skin_path = "";
+    }
+  }
+}
+
+export default uiscript => (uiscript.UI_Character_Emo = Character_Emo);

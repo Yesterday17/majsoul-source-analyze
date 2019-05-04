6402e41890e222617ec9f232f6bc000b253a5e8e
Yesterday17 <t@yesterday17.cn>
2019-05-04 20:32:47 +0800

    UI_Character_Skin

--- src/code.js
+++ src/code.js
@@ -1,64 +1,6 @@
 var uiscript;
 !(function(t) {
   var e = (function() {
-    function e(e) {
-      (this.change_id = 0),
-        (this.skin_path = ""),
-        (this.loaded = false),
-        (this.me = e),
-        (this.img = e),
-        (this.origin_rect = t.UIRect.CreateFromSprite(this.img)),
-        (this.img.skin = "");
-    }
-    return (
-      (e.prototype.setSkin = function(t, e) {
-        var i = this,
-          n = cfg.item_definition.skin.get(t);
-        if (n) {
-          var a = n.path + "/" + e + ".png";
-          if (a != this.skin_path) {
-            (this.skin_path = a), this.change_id++;
-            var r = this.change_id;
-            "" == game.LoadMgr.getResImageSkin(a)
-              ? (this._setLoadedTexture(4e5, e),
-                (this.loaded = false),
-                game.LoadMgr.loadResImage(
-                  [this.skin_path],
-                  Laya.Handler.create(this, function() {
-                    r == i.change_id &&
-                      ((i.loaded = true), i._setLoadedTexture(t, e));
-                  })
-                ))
-              : ((this.loaded = true), this._setLoadedTexture(t, e));
-          }
-        } else this.clear();
-      }),
-      (e.prototype.clear = function() {
-        this.change_id++,
-          this.loaded &&
-            "" != this.skin_path &&
-            (Laya.loader.clearTextureRes(
-              game.LoadMgr.getResImageSkin(this.skin_path)
-            ),
-            (this.loaded = false),
-            (this.skin_path = ""));
-      }),
-      (e.prototype._setLoadedTexture = function(t, e) {
-        var i = cfg.item_definition.skin.get(t);
-        "full" == e || "half" == e
-          ? game.Tools.charaPart(t, this.img, e, this.origin_rect)
-          : (this.img.skin = game.LoadMgr.getResImageSkin(
-              i.path + "/" + e + ".png"
-            ));
-      }),
-      e
-    );
-  })();
-  t.UI_Character_Skin = e;
-})(uiscript || (uiscript = {}));
-var uiscript;
-!(function(t) {
-  var e = (function() {
     function t(t) {
       (this.change_id = 0),
         (this.skin_path = ""),

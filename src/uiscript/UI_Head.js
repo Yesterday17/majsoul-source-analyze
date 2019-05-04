4bde48a8b1318bcd812ad30b2d0b00d3d57872dd
Yesterday17 <t@yesterday17.cn>
2019-05-04 20:57:01 +0800

    UI_Head es6ify

--- src/uiscript/UI_Head.js
+++ src/uiscript/UI_Head.js
@@ -1,46 +1,41 @@
-module.exports = function(uiscript) {
-  var newHead = (function() {
-    function head(e) {
-      this.me = e;
-      this.img_head = new uiscript.UI_Character_Skin(e.getChildAt(0));
-      this.img_emo = this.img_head.me.getChildAt(0);
+class UI_Head {
+  constructor(e) {
+    this.me = e;
+    this.img_head = new uiscript.UI_Character_Skin(e.getChildAt(0));
+    this.img_emo = this.img_head.me.getChildAt(0);
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id) {
+    this._id = id;
+    this.img_head.setSkin(id, "smallhead");
+  }
+
+  setEmo(t, e) {
+    const _this = this;
+    if (e === undefined) {
+      e = -1;
     }
 
-    Object.defineProperty(head.prototype, "id", {
-      get: function() {
-        return this._id;
-      },
-      set: function(id) {
-        this._id = id;
-        this.img_head.setSkin(id, "smallhead");
-      },
-      enumerable: true,
-      configurable: true
-    });
+    Laya.timer.clearAll(this);
 
-    head.prototype.setEmo = function(t, e) {
-      var _this = this;
-      if (e === undefined) {
-        e = -1;
-      }
+    if (this.img_emo) {
+      if (t && t != "") {
+        this.img_emo.visible = true;
 
-      Laya.timer.clearAll(this);
-
-      if (this.img_emo) {
-        if (t && t != "") {
-          this.img_emo.visible = true;
-
-          if (e > 0) {
-            Laya.timer.once(e, this, function() {
-              _this.img_emo.visible = false;
-            });
-          }
-        } else {
-          this.img_emo.visible = false;
+        if (e > 0) {
+          Laya.timer.once(e, this, () => {
+            _this.img_emo.visible = false;
+          });
         }
+      } else {
+        this.img_emo.visible = false;
       }
-    };
-    return head;
-  })();
-  uiscript.UI_Head = newHead;
-};
+    }
+  }
+}
+
+export default uiscript => (uiscript.UI_Head = UI_Head);

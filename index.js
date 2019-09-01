8a699bb4eae2aecfacc1dad7c69f12df6b0e53fa
Yesterday17 <t@yesterday17.cn>
2019-09-01 20:29:01 +0800

    feat: majsoul-code-splitter

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const fs = require('fs')
+import { format } from 'prettier'
 
-const prettier = require('prettier')
-const fetch = require('node-fetch')
-const { transform } = require('lebab')
-
-const version = 'v0.6.28.w'
-
-fetch(`https://www.majsoul.com/1/${version}/code.js`).then(data => data.text()).then(text => {
-  console.log('Fetched code.js')
-
-  const formatted = prettier.format(text, {
+export default code => {
+  const codeAll = {}
+  const formatted = format(code, {
     semi: true,
     arrowParens: 'avoid',
     singleQuote: true,
@@ -17,25 +10,14 @@ fetch(`https://www.majsoul.com/1/${version}/code.js`).then(data => data.text()).
     endOfLine: 'lf',
     parser: 'babel'
   })
-  console.log('Formatted code.js')
 
-  const majsoul_start = formatted.indexOf('var uiscript;')
-  let majsoul_code = formatted.substr(majsoul_start)
-    ; (() => {
-      const laya_code = formatted.substr(0, majsoul_start)
-      fs.writeFileSync('./code/laya.js', laya_code, { encoding: 'utf-8' })
-      console.log('Saved laya.js')
-    })()
+  const majsoulStart = formatted.indexOf('var uiscript;')
+  const majsoulCode = formatted.substr(majsoulStart)
+  codeAll['laya'] = formatted.substr(0, majsoulStart)
 
-  majsoul_code.split('\nvar').map((code, index) => index == 0 ? code : 'var' + code)
-    .map((code, index) => {
-      fs.mkdirSync(`./code/${index}/`)
-      fs.writeFileSync(`./code/${index}/${index}.js`, code, { encoding: 'utf-8' })
-      return code
-    })
-    .map((code) => transform(code, ['arrow', 'arrow-return', 'for-of', 'for-each', 'arg-rest', 'arg-spread', 'obj-method', 'obj-shorthand', 'no-strict', 'exponent', 'multi-var', 'let', 'class', 'commonjs', 'template', 'default-param', 'destruct-param', 'includes']).code)
-    .forEach((code, index) => {
-      fs.writeFileSync(`./code/${index}/${index}_lebab.js`, code, { encoding: 'utf-8' })
-    })
+  codeAll['majsoul'] = majsoulCode
+    .split('\nvar')
+    .map((code, index) => (index === 0 ? code : 'var' + code))
 
-})
+  return codeAll
+}
